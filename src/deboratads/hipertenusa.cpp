90d6796a9070f0e36052c12d7ae7190c626dd1ac
deboratads <devnull@localhost>
2010-04-05 23:45:18 -0300

    Mais um ajuste em hipertenusa e adicionando a solução para pizza.

--- src/deboratads/hipertenusa.cpp
+++ src/deboratads/hipertenusa.cpp
@@ -7,6 +7,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <strings.h>
 
 #define MAX 3170
 #define MAXHIP 10000001
---
+++ src/deboratads/pizza.cpp
@@ -0,0 +1,57 @@
+/*
+ * pizza.cpp
+ *
+ *  Created on: 05/04/2010
+ *      Author: Debora
+ */
+
+#include <algorithm>
+
+#include <stdio.h>
+#include <math.h>
+
+#define MAX 1024
+
+int pz[MAX], pt[MAX];
+double fat;
+
+using namespace std;
+
+bool cabe(int ipz, int ipt) {
+	return (pt[ipt]*fat >= pz[ipz]);
+}
+
+int main() {
+	int m, n, r, nf;
+	fat = 2*cos(acos(-1)/8);
+	while(1) {
+		scanf("%d%d", &m, &n);
+		if (!m && !n) break;
+
+		for (int i = 0; i < m; ++i) {
+			scanf("%d", &pz[i]);
+		}
+		for (int i = 0; i < n; ++i) {
+			scanf("%d", &pt[i]);
+		}
+		sort(pz, pz+m);
+		sort(pt, pt+n);
+		r = 0;
+		nf = 8;
+		int i = 0, j = 0;
+		while (i < m && j < n) {
+			if (cabe(i, j)) {
+				nf--;
+				if (nf == 0) {
+					nf = 8;
+					i++;
+				}
+				j++;
+				r++;
+			} else {
+				j++;
+			}
+		}
+		printf("%d\n", r);
+	}
+}
