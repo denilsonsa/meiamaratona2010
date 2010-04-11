e073edd65e9abfa607e64c8e88bed82db124b2e7
Vinicius <>
2010-04-11 15:33:16 -0300

    Gerador do pizza.

---
+++ tests/pizza/gerador.cpp
@@ -0,0 +1,138 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MASK 0x80000000
+int meurand()
+{
+	return ((rand()<<16) + rand()) & (~MASK);
+}
+int randab(int a, int b)
+{
+	return meurand()%(b-a+1) + a;
+}
+
+	
+void sample()
+{
+	printf("2 10\n");
+	printf("5 5\n");
+	printf("3 3 5 5 5 5 5 5 5 5\n");
+	printf("2 10\n");
+	printf("50 5\n");
+	printf("3 3 5 5 5 5 5 5 5 5\n");
+}
+
+#define MAXM 1000000
+#define MAXN 1000000
+#define MAXR 10000
+
+void grande()
+{
+	int m, n;
+	int i;
+
+	m = MAXM;
+	n = MAXN;
+
+	printf("%d %d\n", m, n);
+
+	for (i=0; i<m; i++)
+	{
+		if (i)
+		{
+			putchar(' ');
+		}
+		printf("%d", randab(1, MAXR));
+	}
+	putchar('\n');
+	for (i=0; i<n; i++)
+	{
+		if (i)
+		{
+			putchar(' ');
+		}
+		printf("%d", randab(1, MAXR));
+	}
+	putchar('\n');
+
+}
+
+
+
+void aleat2(int qt)
+{
+	int m, n;
+	int i;
+
+	while (qt--)
+	{
+		n = randab(MAXN/100, MAXN/10);
+		m = randab(n/3, n);
+
+		printf("%d %d\n", m, n);
+
+		for (i=0; i<m; i++)
+		{
+			if (i)
+			{
+				putchar(' ');
+			}
+			printf("%d", randab(1, MAXR));
+		}
+		putchar('\n');
+		for (i=0; i<n; i++)
+		{
+			if (i)
+			{
+				putchar(' ');
+			}
+			printf("%d", randab(1, MAXR));
+		}
+		putchar('\n');
+	}
+}
+
+void aleat3(int qt)
+{
+	int m, n;
+	int i;
+
+	while (qt--)
+	{
+
+		m = randab(1, MAXM/1000);
+		n = randab(1, MAXN/100);
+
+		printf("%d %d\n", m, n);
+
+		for (i=0; i<m; i++)
+		{
+			if (i)
+			{
+				putchar(' ');
+			}
+			printf("%d", randab(1, MAXR));
+		}
+		putchar('\n');
+		for (i=0; i<n; i++)
+		{
+			if (i)
+			{
+				putchar(' ');
+			}
+			printf("%d", randab(1, MAXR));
+		}
+		putchar('\n');
+	}
+}
+
+int main()
+{
+	sample();
+	grande();
+	aleat2(10);
+	aleat3(40);
+
+	return 0;
+}
